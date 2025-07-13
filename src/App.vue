93718b598d2e5536267767d134da6dceabd85c27
Ifeanyi Ofikulu <ifeanyiofiks@gmail.com>
2025-07-13 19:10:54 +0100

    analytics

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
+import { Analytics } from '@vercel/analytics/vue';
 import Portfolio from './page/Portfolio.vue';
 
 </script>
 
 <template>
+  <Analytics />
   <Portfolio />
 </template>
 
